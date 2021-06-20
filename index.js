3afcf85ecb59d535a0ef30b6f87c2577ddd5ef68
davidwpacker <davidwgpacker@gmail.com>
2021-06-20 12:56:45 +1000

    new btn which draws 2 cards using the fetch API and previous function

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 
-//  a button that, when clicked, gets a new deck of cards from the deckofcards API
-document.getElementById("new-deck").addEventListener("click", function() {
-    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+let deckId
+
+function handleClick() {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/") //  a button that, when clicked, gets a new deck of cards from the deckofcards API
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            deckId = data.deck_id
+        })
+}
+
+document.getElementById("new-deck").addEventListener("click", handleClick)
+
+document.getElementById("draw-cards").addEventListener("click", () => {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => console.log(data))
 })
-
